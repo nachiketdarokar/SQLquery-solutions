f4eb14bfa867332b3c62a5ffd2bccbe3b083ac99
nachiketdarokar <darokarnachiket1999@gmail.com>
2024-04-06 20:08:44 +0530

    LEETCODE problems & solution on SQL

---
+++ leetcode_solutions.sql
@@ -0,0 +1,90 @@
+#LEETCODE QUERY & SOLUTIONS
+create database leetcode;
+use leetcode;
+
+# Write an SQL query to report the number of unique subjects each teacher teaches in the university. 
+#Return the result table in any order. The query result format is shown in the following example.
+#This question was asked in a Capgemini company interview.
+select teacher_id,count(distinct subject_id) as cnt from Teacher
+group by teacher_id;
+
+/*Table: Visits
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| visit_id    | int     |
+| customer_id | int     |
++-------------+---------+
+visit_id is the column with unique values for this table.
+This table contains information about the customers who visited the mall.
+ 
+
+Table: Transactions
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| transaction_id | int     |
+| visit_id       | int     |
+| amount         | int     |
++----------------+---------+
+transaction_id is column with unique values for this table.
+This table contains information about the transactions made during the visit_id.
+ 
+
+Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+Return the result table sorted in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Visits
++----------+-------------+
+| visit_id | customer_id |
++----------+-------------+
+| 1        | 23          |
+| 2        | 9           |
+| 4        | 30          |
+| 5        | 54          |
+| 6        | 96          |
+| 7        | 54          |
+| 8        | 54          |
++----------+-------------+
+Transactions
++----------------+----------+--------+
+| transaction_id | visit_id | amount |
++----------------+----------+--------+
+| 2              | 5        | 310    |
+| 3              | 5        | 300    |
+| 9              | 5        | 200    |
+| 12             | 1        | 910    |
+| 13             | 2        | 970    |
++----------------+----------+--------+
+Output: 
++-------------+----------------+
+| customer_id | count_no_trans |
++-------------+----------------+
+| 54          | 2              |
+| 30          | 1              |
+| 96          | 1              |
++-------------+----------------+
+Explanation: 
+Customer with id = 23 visited the mall once and made one transaction during the visit with id = 12.
+Customer with id = 9 visited the mall once and made one transaction during the visit with id = 13.
+Customer with id = 30 visited the mall once and did not make any transactions.
+Customer with id = 54 visited the mall three times. During 2 visits they did not make any transactions, and during one visit they made 3 transactions.
+Customer with id = 96 visited the mall once and did not make any transactions.
+As we can see, users with IDs 30 and 96 visited the mall one time without making any transactions. Also, user 54 visited the mall twice and did not make any transactions.
+*/
+with notran as (select v.customer_id as customer_id ,count(t.transaction_id) as cnt from Visits v
+	left join Transactions t on t.visit_id=v.visit_id 
+	group by v.visit_id,customer_id
+	having cnt=0)
+select customer_id,count(cnt) as count_no_trans from notran
+group by customer_id;
